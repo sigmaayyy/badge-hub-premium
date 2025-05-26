84d9599d4d97c30f06f13b29ec296c77682c9c06
sigmaayyy <vicedokpolor2@outlook.com>
2025-05-26 13:45:42 +0100

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -7,7 +7,7 @@ local Window = Rayfield:CreateWindow({
    LoadingSubtitle = "Loading Content...",
    Theme = "DarkBlue", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-   ToggleUIKeybind = "K" -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+   
 
    DisableRayfieldPrompts = false,
    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
@@ -75,7 +75,7 @@ end
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Credits to Ninja & nexer",
 	Text = "Made by @NinjaKid-k5q and nexer1234 ( youtube & discord )",
-        Icon = "rbxassetid://125704683916878",
+        Icon = nil,
 	Duration = 36000,
 	Button1 = "I'll take a look at the channels..."
 })
@@ -205,7 +205,7 @@ end
 
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Credits to Nexer1234_AnotherAlt",
+	Title = "Credits to Ninja",
 	Text = "Made by @NinjaKid-k5q ( youtube & discord )",
         Icon = nil,
 	Duration = 36000,
@@ -259,6 +259,378 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Message from Ninja",
 	Text = "Wrong game buddy",
     Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+end
+   end,
+})
+
+local BadgeButton = BadgeTab:CreateButton({
+   Name = "Get Avatar & Relude",
+   Callback = function()
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+
+
+
+function Checkamogus()
+DontGetAvatar = false
+repeat task.wait() until DontGetAvatar ~= nil
+
+if DontGetAvatar == true then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3258, -65, 824)
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+wait(5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace["the cube of life"].Part.CFrame
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+wait(5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace["Big Heart"].CFrame
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+wait(5)
+end
+
+end
+
+-- Credits, message is not squished up cuz it looks bad
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Credits to Ninja",
+	Text = "Made by @NinjaKid-k5q ( youtube & discord )",
+        Icon = nil,
+	Duration = 36000,
+	Button1 = "Thanks!"
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Please re-execute script after teleporting",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+
+
+if game.PlaceId == 18550498098 then
+
+Checkamogus()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3258, -62, 824)
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+wait(5)
+for i,v in pairs(workspace:GetDescendants()) do
+if v.Name == "ShackLever" and v:FindFirstChildWhichIsA("ClickDetector") then
+fireclickdetector(v:FindFirstChildWhichIsA("ClickDetector"))
+end
+end
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3258, -62, 824)
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+repeat task.wait(.1) until workspace.Map.Components:FindFirstChild("GuideNPC")
+task.wait(.8)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(599, 250, -322)
+task.wait(.8)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+
+while task.wait(.1) do
+if game.Players.LocalPlayer.Backpack:FindFirstChild("Lantern") then
+game.Players.LocalPlayer.Backpack.Lantern.Parent = game.Players.LocalPlayer.Character
+end
+game:GetService("ReplicatedStorage").Remotes.Data.AnswerInCutscene:FireServer("Spare")
+
+for i,v in pairs(workspace:GetChildren()) do
+
+if v.Name == "PusherWall" then
+v.CanCollide = false
+end
+
+if v.Name == "DungeonGolem" and v:FindFirstChild("Cube.001") then
+if game.Players.LocalPlayer.Character:FindFirstChild("Lantern") and game.Players.LocalPlayer.Character.Lantern:FindFirstChild("Network") then
+game.Players.LocalPlayer.Character.Lantern:Activate()
+game.Players.LocalPlayer.Character.Lantern.Network:FireServer("Hit", v["Cube.001"])
+end
+end
+
+if v.Name == "golem" and v:FindFirstChild("Hitbox") then
+if game.Players.LocalPlayer.Character:FindFirstChild("Lantern") and game.Players.LocalPlayer.Character.Lantern:FindFirstChild("Network") then
+game.Players.LocalPlayer.Character.Lantern:Activate()
+game.Players.LocalPlayer.Character.Lantern.Network:FireServer("Hit", v.Hitbox)
+end
+end
+
+if v.Name == "ReplicaNPC" and v:FindFirstChild("HumanoidRootPart") then
+if game.Players.LocalPlayer.Character:FindFirstChild("Lantern") and game.Players.LocalPlayer.Character.Lantern:FindFirstChild("Network") then
+game.Players.LocalPlayer.Character.Lantern:Activate()
+game.Players.LocalPlayer.Character.Lantern.Network:FireServer("Hit", v.HumanoidRootPart)
+end
+end
+
+if v.Name == "GuideNPC" and v:FindFirstChild("HumanoidRootPart") then
+if game.Players.LocalPlayer.Character:FindFirstChild("Lantern") and game.Players.LocalPlayer.Character.Lantern:FindFirstChild("Network") then
+game.Players.LocalPlayer.Character.Lantern:Activate()
+game.Players.LocalPlayer.Character.Lantern.Network:FireServer("Hit", v.HumanoidRootPart)
+end
+end
+
+if v.Name == "TrackGloveMissile" then
+if game.Players.LocalPlayer.Character:FindFirstChild("Lantern") and game.Players.LocalPlayer.Character.Lantern:FindFirstChild("Network") then
+game.Players.LocalPlayer.Character.Lantern:Activate()
+game.Players.LocalPlayer.Character.Lantern.Network:FireServer("Hit", v)
+end
+end
+
+end
+end
+
+  
+elseif game.PlaceId == 11520107397 or game.PlaceId == 9015014224 or game.PlaceId == 6403373529 or game.PlaceId == 124596094333302 then
+
+Instance.new("Message",game.CoreGui).Text = "You're being teleported, please wait, it may take up to a minute..."
+
+for i,v in pairs(workspace.BountyHunterRoom:GetChildren()) do
+if v and v:IsA('Model') and v:FindFirstChild('Meshes/boxshadow_Cube.005') then
+if v["Meshes/boxshadow_Cube.005"].PlayerCounter.SurfaceGui.TextLabel.Text == "0/4 Hunters" then
+if v["Meshes/boxshadow_Cube.005"].Counter.SurfaceGui.TextLabel.Text ~= "1" then
+repeat task.wait(.1) until v["Meshes/boxshadow_Cube.005"].Counter.SurfaceGui.TextLabel.Text == "1"
+end
+while task.wait() do game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v["Meshes/boxshadow_Cube.005"].Hitbox.CFrame end
+else
+repeat task.wait(.1) until v["Meshes/boxshadow_Cube.005"].PlayerCounter.SurfaceGui.TextLabel.Text == "0/4 Hunters"
+if v["Meshes/boxshadow_Cube.005"].Counter.SurfaceGui.TextLabel.Text ~= "1" then
+repeat task.wait(.1) until v["Meshes/boxshadow_Cube.005"].Counter.SurfaceGui.TextLabel.Text == "1"
+end
+while task.wait() do game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v["Meshes/boxshadow_Cube.005"].Hitbox.CFrame end
+end
+end
+end
+
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Wrong game buddy",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+end
+   end,
+})
+
+local BadgeButton = BadgeTab:CreateButton({
+   Name = "Get Hybrid",
+   Callback = function()
+if not game.IsLoaded then
+game.Loaded:Wait()
+end
+
+-- Credits, message is not squished up cuz it looks bad
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Credits to Ninja",
+	Text = "Made by @NinjaKid-k5q ( youtube & discord )",
+        Icon = nil,
+	Duration = 36000,
+	Button1 = "Thanks!"
+})
+
+if game.PlaceId == 11520107397 or game.PlaceId == 9015014224 or game.PlaceId == 6403373529 or game.PlaceId == 124596094333302 then
+  
+game:GetService("ReplicatedStorage").GRRRRR:FireServer(true)
+  
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Wrong game buddy",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+end
+   end,
+})
+
+local BadgeButton = BadgeTab:CreateButton({
+   Name = "Get Elude & Counter",
+   Callback = function()
+   if not game.IsLoaded then
+game.Loaded:Wait()
+end
+
+-- Credits, message is not squished up cuz it looks bad
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Credits to Ninja",
+	Text = "Made by @NinjaKid-k5q ( youtube & discord )",
+        Icon = nil,
+	Duration = 36000,
+	Button1 = "Thanks!"
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Please re-execute script after teleporting",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+
+
+if game.PlaceId == 11828384869 then
+
+for i,v in pairs(workspace:GetDescendants()) do
+if v:IsA("ClickDetector") then
+fireclickdetector(v)
+end
+end
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1000,1000,1000)
+wait(0.6)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+Time = 120
+repeat task.wait() until Time ~= nil
+for i = 1,Time do
+Time = Time - 1
+Instance.new("Message",workspace).Text = "Time left to receive: "..Time
+wait(1)
+workspace:FindFirstChildWhichIsA("Message"):Destroy()
+end
+task.wait(.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+wait(1)
+while task.wait() do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.Ruins.Elude.Glove.CFrame
+for i,v in pairs(workspace:GetDescendants()) do
+if v:IsA("ClickDetector") then
+fireclickdetector(v)
+end
+end
+end
+  
+elseif game.PlaceId == 11520107397 or game.PlaceId == 9015014224 or game.PlaceId == 6403373529 or game.PlaceId == 124596094333302 then
+  
+while task.wait() do game:GetService("TeleportService"):Teleport(11828384869) end
+
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Wrong game buddy",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+end
+   end,
+})
+
+local BadgeButton = BadgeTab:CreateButton({
+   Name = "Get Fan & Boxer",
+   Callback = function()
+   if not game.IsLoaded then
+game.Loaded:Wait()
+end
+
+-- Credits, message is not squished up cuz it looks bad
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Credits to Nexer1234_AnotherAlt",
+	Text = "Made by @nNinjaKid-k5q ( youtube & discord )",
+        Icon = nil,
+	Duration = 36000,
+	Button1 = "Thanks!"
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Please re-execute script after teleporting",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+
+
+if game.PlaceId == 7234087065 then
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.QuestStuff.Key.CFrame
+task.wait(1.5)
+game:GetService("ReplicatedStorage").RemoteEvents.SuitUpClown:FireServer()
+task.wait(.25)
+game:GetService("ReplicatedStorage").RemoteEvents.KeyQuest:FireServer()
+task.wait(.25)
+game:GetService("ReplicatedStorage").RemoteEvents.KeyAcquired:FireServer()
+task.wait(.25)
+game:GetService("ReplicatedStorage").RemoteEvents.GOHOME:FireServer()
+task.wait(.25)
+game:GetService("ReplicatedStorage").RemoteEvents.KeyBadgeReward:FireServer()
+task.wait(1.5)
+if workspace:FindFirstChild("BoxingGloves") == nil then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.Buildings.house.TP.CFrame * CFrame.new(0,10,0)
+repeat task.wait(.1) until workspace:FindFirstChild("BoxingGloves")
+task.wait(1.5)
+fireclickdetector(workspace.BoxingGloves.ClickDetector)
+else
+fireclickdetector(workspace.BoxingGloves.ClickDetector)
+end
+  
+elseif game.PlaceId == 11520107397 or game.PlaceId == 9015014224 or game.PlaceId == 6403373529 or game.PlaceId == 124596094333302 then
+  
+while task.wait() do game:GetService("TeleportService"):Teleport(7234087065) end
+
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Wrong game buddy",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+end
+   end,
+})
+
+local BadgeButton = BadgeTab:CreateButton({
+   Name = "Get Bind",
+   Callback = function()
+   if not game.IsLoaded then
+game.Loaded:Wait()
+end
+
+-- Credits, message is not squished up cuz it looks bad
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Credits to Ninja",
+	Text = "Made by @NinjaKid-k5q ( youtube & discord )",
+        Icon = nil,
+	Duration = 36000,
+	Button1 = "Thanks!"
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Ninja",
+	Text = "Please re-execute script after teleporting",
+    Icon = nil,
+	Duration = 36000,
+	Button1 = "Oh..."
+})
+
+
+if game.PlaceId == 74169485398268 then
+
+local time = tick()
+while tick() - time < 10 do task.wait() fireclickdetector(workspace.Orb:FindFirstChildWhichIsA("ClickDetector")) end
+while task.wait() do game:GetService("TeleportService"):Teleport(6403373529) end
+
+elseif game.PlaceId == 11520107397 or game.PlaceId == 9015014224 or game.PlaceId == 6403373529 or game.PlaceId == 124596094333302 then
+  
+while task.wait() do game:GetService("TeleportService"):Teleport(74169485398268) end
+
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Message from Nexer1234_AnotherAlt",
+	Text = "Wrong game buddy",
+    Icon = "rbxassetid://125704683916878",
 	Duration = 36000,
 	Button1 = "Oh..."
 })
